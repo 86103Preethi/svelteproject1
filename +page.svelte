f146d202f02373b16f574ac06a9c68df34133e22
Preethi S <84867260+86103Preethi@users.noreply.github.com>
2023-07-18 18:35:34 +0530

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,34 @@
+
+
+<main class='min-h-screen bg-gradient-to-r from-purple-400 via-purple-200 to-pink-300'>
+    <nav class="bg-gray-800 p-4">
+        <ul class="flex">
+            <li class="mr-20">
+                <a href="/" class="text-white hover:text-gray-300">Home</a>
+            </li>
+            <li class="mr-20">
+                <a href="/about" class="text-white hover:text-gray-300">About</a>
+            </li>
+            <li class="mr-20">
+                <a href="/register" class="text-white hover:text-gray-300">Register</a>
+            </li>
+            <li class="mr-20">
+                <a href="/login" class="text-white hover:text-gray-300">Login</a>
+            </li>
+            <li class="mr-20">
+                <a href="/contact" class="text-white hover:text-gray-300">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</main>
+
+
+
+
+
+<style>
+
+</style>
+
+
+
